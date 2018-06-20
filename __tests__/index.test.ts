ded34ec8bd4b24b07e1af6dee592c7bfe86d368d
Chris Guevara <chrismguevara@gmail.com>
2018-06-19 20:32:41 -0700

    Refactor tests to use describe blocks

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -12,106 +12,164 @@ interface TestInput {
     output: string
 };
 
-const testInputs: TestInput[] = [
-    {
-        input: { meaning: 'to see', kanji: '見る', hiragana: 'みる', verbType: JapaneseVerbType.RU_VERB },
-        output: '見ます'
-    },
-    {
-        input: { meaning: 'to hear', kanji: '聞く', hiragana: 'きく', verbType: JapaneseVerbType.U_VERB },
-        output: '聞きます'
-    },
-    {
-        input: { meaning: 'to eat', kanji: '食べる', hiragana: 'たべる', verbType: JapaneseVerbType.RU_VERB },
-        output: '食べます'
-    },
-    {
-        input: { meaning: 'to swim', kanji: '泳ぐ', hiragana: 'およぐ', verbType: JapaneseVerbType.U_VERB },
-        output: '泳ぎます'
-    }
-];
+describe('getFormalPresentAffirmative', () => {
 
-test('getFormalPresentAffirmative', () => {
-    testInputs.forEach((testInput: TestInput) => {
-        const actual = getFormalPresentAffirmative(testInput.input);
-        expect(actual).toBe(testInput.output);
+    test('works with ru verbs', () => {
+        const inputs: TestInput[] = [
+            {
+                input: { meaning: 'to see', kanji: '見る', hiragana: 'みる', verbType: JapaneseVerbType.RU_VERB },
+                output: '見ます'
+            },
+            {
+                input: { meaning: 'to eat', kanji: '食べる', hiragana: 'たべる', verbType: JapaneseVerbType.RU_VERB },
+                output: '食べます'
+            }
+        ];
+        inputs.forEach((input: TestInput) => {
+            const actual = getFormalPresentAffirmative(input.input);
+            expect(actual).toBe(input.output);
+        });
     });
+
+    test('works with u verbs', () => {
+        const inputs: TestInput[] = [
+            {
+                input: { meaning: 'to hear', kanji: '聞く', hiragana: 'きく', verbType: JapaneseVerbType.U_VERB },
+                output: '聞きます'
+            },
+            {
+                input: { meaning: 'to swim', kanji: '泳ぐ', hiragana: 'およぐ', verbType: JapaneseVerbType.U_VERB },
+                output: '泳ぎます'
+            }
+        ];
+        inputs.forEach((input: TestInput) => {
+            const actual = getFormalPresentAffirmative(input.input);
+            expect(actual).toBe(input.output);
+        });
+    });
+
+    // TODO works with irregular verbs
 });
 
-const testInputs2: TestInput[] = [
-    {
-        input: { meaning: 'to see', kanji: '見る', hiragana: 'みる', verbType: JapaneseVerbType.RU_VERB },
-        output: '見ません'
-    },
-    {
-        input: { meaning: 'to hear', kanji: '聞く', hiragana: 'きく', verbType: JapaneseVerbType.U_VERB },
-        output: '聞きません'
-    },
-    {
-        input: { meaning: 'to eat', kanji: '食べる', hiragana: 'たべる', verbType: JapaneseVerbType.RU_VERB },
-        output: '食べません'
-    },
-    {
-        input: { meaning: 'to swim', kanji: '泳ぐ', hiragana: 'およぐ', verbType: JapaneseVerbType.U_VERB },
-        output: '泳ぎません'
-    }
-];
+describe('getFormalPresentNegative', () => {
 
-test('getFormalPresentNegative', () => {
-    testInputs2.forEach((testInput: TestInput) => {
-        const actual = getFormalPresentNegative(testInput.input);
-        expect(actual).toBe(testInput.output);
+    test('works with ru verbs', () => {
+        const inputs: TestInput[] = [
+            {
+                input: { meaning: 'to see', kanji: '見る', hiragana: 'みる', verbType: JapaneseVerbType.RU_VERB },
+                output: '見ません'
+            },
+            {
+                input: { meaning: 'to eat', kanji: '食べる', hiragana: 'たべる', verbType: JapaneseVerbType.RU_VERB },
+                output: '食べません'
+            }
+        ];
+
+        inputs.forEach((input: TestInput) => {
+            const actual = getFormalPresentNegative(input.input);
+            expect(actual).toBe(input.output);
+        });
     });
+
+    test('works with u verbs', () => {
+        const inputs: TestInput[] = [
+            {
+                input: { meaning: 'to hear', kanji: '聞く', hiragana: 'きく', verbType: JapaneseVerbType.U_VERB },
+                output: '聞きません'
+            },
+            {
+                input: { meaning: 'to swim', kanji: '泳ぐ', hiragana: 'およぐ', verbType: JapaneseVerbType.U_VERB },
+                output: '泳ぎません'
+            }
+        ];
+
+        inputs.forEach((input: TestInput) => {
+            const actual = getFormalPresentNegative(input.input);
+            expect(actual).toBe(input.output);
+        });
+    });
+    
+    // TODO works with irregular verbs
 });
 
-const testInputs3: TestInput[] = [
-    {
-        input: { meaning: 'to see', kanji: '見る', hiragana: 'みる', verbType: JapaneseVerbType.RU_VERB },
-        output: '見ました'
-    },
-    {
-        input: { meaning: 'to hear', kanji: '聞く', hiragana: 'きく', verbType: JapaneseVerbType.U_VERB },
-        output: '聞きました'
-    },
-    {
-        input: { meaning: 'to eat', kanji: '食べる', hiragana: 'たべる', verbType: JapaneseVerbType.RU_VERB },
-        output: '食べました'
-    },
-    {
-        input: { meaning: 'to swim', kanji: '泳ぐ', hiragana: 'およぐ', verbType: JapaneseVerbType.U_VERB },
-        output: '泳ぎました'
-    }
-];
+describe('getFormalPastAffirmative', () => {
 
-test('getFormalPastAffirmative', () => {
-    testInputs3.forEach((testInput: TestInput) => {
-        const actual = getFormalPastAffirmative(testInput.input);
-        expect(actual).toBe(testInput.output);
+    test('works with ru verbs', () => {
+        const inputs: TestInput[] = [
+            {
+                input: { meaning: 'to see', kanji: '見る', hiragana: 'みる', verbType: JapaneseVerbType.RU_VERB },
+                output: '見ました'
+            },
+            {
+                input: { meaning: 'to eat', kanji: '食べる', hiragana: 'たべる', verbType: JapaneseVerbType.RU_VERB },
+                output: '食べました'
+            }
+        ];
+
+        inputs.forEach((input: TestInput) => {
+            const actual = getFormalPastAffirmative(input.input);
+            expect(actual).toBe(input.output);
+        });
     });
+
+    test('works with u verbs', () => {
+        const inputs: TestInput[] = [
+            {
+                input: { meaning: 'to hear', kanji: '聞く', hiragana: 'きく', verbType: JapaneseVerbType.U_VERB },
+                output: '聞きました'
+            },
+            {
+                input: { meaning: 'to swim', kanji: '泳ぐ', hiragana: 'およぐ', verbType: JapaneseVerbType.U_VERB },
+                output: '泳ぎました'
+            }
+        ];
+
+        inputs.forEach((input: TestInput) => {
+            const actual = getFormalPastAffirmative(input.input);
+            expect(actual).toBe(input.output);
+        });
+    });
+    
+    // TODO works with irregular verbs
 });
 
-const testInputs4: TestInput[] = [
-    {
-        input: { meaning: 'to see', kanji: '見る', hiragana: 'みる', verbType: JapaneseVerbType.RU_VERB },
-        output: '見ませんでした'
-    },
-    {
-        input: { meaning: 'to hear', kanji: '聞く', hiragana: 'きく', verbType: JapaneseVerbType.U_VERB },
-        output: '聞きませんでした'
-    },
-    {
-        input: { meaning: 'to eat', kanji: '食べる', hiragana: 'たべる', verbType: JapaneseVerbType.RU_VERB },
-        output: '食べませんでした'
-    },
-    {
-        input: { meaning: 'to swim', kanji: '泳ぐ', hiragana: 'およぐ', verbType: JapaneseVerbType.U_VERB },
-        output: '泳ぎませんでした'
-    }
-];
+describe('getFormalPastNegative', () => {
 
-test('getFormalPastNegative', () => {
-    testInputs4.forEach((testInput: TestInput) => {
-        const actual = getFormalPastNegative(testInput.input);
-        expect(actual).toBe(testInput.output);
+    test('works with ru verbs', () => {
+        const inputs: TestInput[] = [
+            {
+                input: { meaning: 'to see', kanji: '見る', hiragana: 'みる', verbType: JapaneseVerbType.RU_VERB },
+                output: '見ませんでした'
+            },
+            {
+                input: { meaning: 'to eat', kanji: '食べる', hiragana: 'たべる', verbType: JapaneseVerbType.RU_VERB },
+                output: '食べませんでした'
+            }
+        ];
+
+        inputs.forEach((input: TestInput) => {
+            const actual = getFormalPastNegative(input.input);
+            expect(actual).toBe(input.output);
+        });
     });
+
+    test('works with u verbs', () => {
+        const inputs: TestInput[] = [
+            {
+                input: { meaning: 'to hear', kanji: '聞く', hiragana: 'きく', verbType: JapaneseVerbType.U_VERB },
+                output: '聞きませんでした'
+            },
+            {
+                input: { meaning: 'to swim', kanji: '泳ぐ', hiragana: 'およぐ', verbType: JapaneseVerbType.U_VERB },
+                output: '泳ぎませんでした'
+            }
+        ];
+
+        inputs.forEach((input: TestInput) => {
+            const actual = getFormalPastNegative(input.input);
+            expect(actual).toBe(input.output);
+        });
+    });
+    
+    // TODO works with irregular verbs
 });
